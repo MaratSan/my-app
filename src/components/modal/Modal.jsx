a2f6515822bc6ab37e5b55f9b5dee44e9f513f77
Marat <san.marat93@gmail.com>
2023-12-08 10:57:39 +0100

    pop up and initial state

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+// Modal.jsx
+import React, { useState, useEffect } from 'react';
+import { IoSaveOutline } from 'react-icons/io5';
+import { IoCloseOutline } from "react-icons/io5";
+import TodoItem from '../screens/Home/item/TodoItem';
 
-const Modal =({active, setActive}) => {
+const Modal = ({ isOpen, onClose, onSave, todo }) => {
+	const [newTitle, setNewTitle] = useState('');
+	//const [initialTitle, setInitialTitle] = useState('')
+
+	useEffect(() => {
+		if (isOpen) {
+			setNewTitle(todo.title)
+		}
+	}, [isOpen, todo]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (newTitle.trim().length > 0) {
+			onSave(newTitle);
+			onClose();
+		}
+	};
+
 	return (
-		<div className='modal'>
-			<div className='modal__container'>
-
-
+		<div className={`modal ${isOpen ? 'block' : 'hidden'} 
+			fixed inset-0 bg-black bg-opacity-50 z-50`}>
+			<div className="modal-content mx-auto mt-10 bg-gray-800 rounded-lg p-6 w-1/2">
+				<div className=' mt-0 flex justify-end'>
+				<IoCloseOutline className="close text-gray-500 cursor-pointer" onClick={onClose}></IoCloseOutline>
+					</div>
+				<form onSubmit={handleSubmit} className="mt-4">
+					<input
+						type="text"
+						value={newTitle}
+						onChange={(e) => setNewTitle(e.target.value)}
+						placeholder="Введите новый текст задачи"
+						className="border text-white border-gray-300 p-2 w-full rounded bg-gray-800"
+					/>
+					<div className="mt-2 flex justify-end">
+					<button type="submit" className="mt-2 object-right bg-pink-400 text-white py-2 px-4 rounded">
+						<IoSaveOutline size={22} />
+					</button>
+						</div>
+				</form>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
 export default Modal;
--- src/components/screens/Home/item/TodoItem.jsx
+++ src/components/screens/Home/item/TodoItem.jsx
@@ -1,58 +1,33 @@
 import CheckBox from './CheckBox';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import cn from 'classnames';
-import {FaRegTrashAlt} from 'react-icons/fa';
-import {FaRegEdit} from 'react-icons/fa';
-import { IoSaveOutline } from "react-icons/io5";
-import {useDispatch} from 'react-redux';
-import {deleteTodo, updateTodo} from '../../../../app/features/todosSlice';
-import Modal from '../../../modal/Modal'
+import { FaRegTrashAlt } from 'react-icons/fa';
+import { FaRegEdit } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
+import { deleteTodo, updateTodo } from '../../../../app/features/todosSlice';
+import Modal from '../../../modal/Modal';
 
-const TodoItem = ({todo}) => {
-	const [title, setTitle] = useState(todo.title);
-	const [isEdit, setEdit] = useState(false)
-	const dispatch = useDispatch()
-	const [modalActive, setModalActive] = useState(true)
+const TodoItem = ({ todo }) => {
+	const dispatch = useDispatch();
+	const [isModalOpen, setModalOpen] = useState(false);
+	const [newTitle, setNewTitle] = useState(todo.title);
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
+	const handleEditClick = () => {
+		setModalOpen(true);
+	};
 
-		if (title.trim().length === 0)
-			return;
+	const handleEditSave = (title) => {
+		dispatch(updateTodo({
+			...todo,
+			title: title,
+		}));
+	};
 
-		dispatch(updateTodo(
-			{
-				...todo,
-				title: title
-			}
-		));
-		setEdit(false)
-	}
-
-	if (isEdit)
-		return (
-			<div className='mb-2 rounded-2xl bg-gray-800 p-5 w-full'>
-				<form onSubmit={handleSubmit} className='flex items-center justify-between'>
-					<input className='flex items-center justify-between mb-2 bg-gray-800 rounded-2xl px-5 py-3 w-full mr-2'
-						   type='text'
-						   onChange={(e) => setTitle(e.target.value)}
-						   value={title}
-					/>
-					<div className='flex space-x-4'>
-						<button type="submit">
-							{/* icon save */}
-							<IoSaveOutline
-								size={22}
-								className='text-gray-900 hover:text-pink-400 transition-colors ease-in-out duration-300'
-							/>
-						</button>
-					</div>
-				</form>
-			</div>
-		);
+	const handleEditClose = () => {
+		setModalOpen(false);
+	};
 
 	return (
-		<div>
 		<div className='flex items-center justify-between mb-2 rounded-2xl bg-gray-800 p-5 w-full'>
 			<button
 				className='flex items-center'
@@ -60,7 +35,7 @@ const TodoItem = ({todo}) => {
 					updateTodo(
 						{
 							...todo,
-							isCompleted: !todo.isCompleted
+							isCompleted: !todo.isCompleted,
 						}
 					)
 				)}
@@ -68,35 +43,34 @@ const TodoItem = ({todo}) => {
 				<CheckBox isCompleted={todo.isCompleted} />
 				<span
 					className={cn('ml-4 text-lg font-semibold', {
-						'line-through': todo.isCompleted
+						'line-through': todo.isCompleted,
 					})}
 				>
-                    {todo.title}
-				</span>
+          {todo.title}
+        </span>
 			</button>
 			<div className='flex space-x-4'>
-				<button
-					onClick={() => dispatch(deleteTodo(todo))}>
+				<button onClick={() => dispatch(deleteTodo(todo))}>
 					<FaRegTrashAlt
 						size={22}
 						className='text-gray-900 hover:text-pink-400 transition-colors ease-in-out duration-300'
 					/>
 				</button>
-				<button
-					onClick={() => (setEdit(true))}>
+				<button onClick={handleEditClick}>
 					<FaRegEdit
 						size={22}
 						className='text-gray-900 hover:text-pink-400
-				    transition-colors ease-in-out duration-300'
+            transition-colors ease-in-out duration-300'
 					/>
 				</button>
 			</div>
-			<Modal
-				className='h-max w-max bg-blend-overlay position'
-				active={modalActive} setActive={setModalActive}>
 
-			</Modal>
-		</div>
+			<Modal
+				isOpen={isModalOpen}
+				onClose={handleEditClose}
+				onSave={handleEditSave}
+				todo={todo}
+			/>
 		</div>
 	);
 };
