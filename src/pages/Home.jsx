c167390dff790cc71bcd37c08427b88679bf80f5
Marat <san.marat93@gmail.com>
2024-01-10 13:01:51 +0100

    newComm

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
 	return (
 		<div className='text-white w-4/5 mx-auto'>
 			<h1 className='font-bold text-center mb-8'>
-				Todo Marat
+				Todo Marat, better work
 			</h1>
 			<CreateTodoForm />
 			<TodoList/>
